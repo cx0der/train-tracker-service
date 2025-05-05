828d8730f4675ab6b8ebceb8ff3ef3f95a49aa63
cx0der <277487+cx0der@users.noreply.github.com>
2025-05-05 21:08:03 +0100

    [setup] executable jar
    update gradle version
    update junit version
    add gson as a runtime dependency
    add mockito as development dependency
    add fatjar gradle task to build executable jar
    add Main class and main method

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,44 @@
 plugins {
-    id("java")
+    application
 }
 
 group = "blog.devrandom"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    implementation("com.google.code.gson:gson:2.13.0")
+    testImplementation(platform("org.junit:junit-bom:5.12.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.16.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass = "blog.devrandom.Main"
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "processResources"))
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes(mapOf("Main-Class" to application.mainClass))
+        }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
 }
---
+++ src/main/java/blog/devrandom/Main.java
@@ -0,0 +1,7 @@
+package blog.devrandom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Train Tracker version: 1.0.0");
+    }
+}
